5190ef0df78ff4fefb3cfdd839bed1ee8fed2a2c
Patrick McNaughton <65691549+PatMcN7@users.noreply.github.com>
2025-07-24 01:12:47 -0400

    AprilTagDetector

---
+++ unity/Assets/QuestNav/Core/ApriltagDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.QuestNav.Core
+{
+    public class ApriltagDetector : MonoBehaviour
+    {
+        private WebCamTexture leftCamera;
+        private WebCamTexture rightCamera;
+        private Matrix4x4 leftIntrinsics;
+        private Matrix4x4 rightIntrinsics;
+
+        // Use this for initialization
+        void Start()
+        {
+            leftCamera = new WebCamTexture(WebCamTexture.devices[0].name);
+            rightCamera = new WebCamTexture(WebCamTexture.devices[1].name);
+            leftCamera.Play();
+            rightCamera.Play();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
